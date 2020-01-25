011bf40c636a44201b4ae3b482316294b323778f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-26 00:19:25 +0400

    Rubocop at work

--- spec/smart_buttons_spec.rb
+++ spec/smart_buttons_spec.rb
@@ -2,22 +2,52 @@
 
 RSpec.describe SmartButtons do
   it 'returns delete button HTML' do
-    html = '<form action="/delete_this_object" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="_method" value="delete" /><button name="button" type="submit"></button></form>'
-    expect(delete_button('/delete_this_object')).to eq html
+    html = <<-HTML
+      <form action="/delete_this_object" accept-charset="UTF-8" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <input type="hidden" name="_method" value="delete" />
+        <button name="button" type="submit"></button>
+      </form>
+    HTML
+    expect(delete_button('/delete_this_object')).to eq html.to_s.gsub(/\n\s\s*/, '').gsub(/^\s\s*/, '').gsub("\n", '')
   end
 
   it 'returns delete button HTML with HTML inside' do
-    html = '<form action="/delete_this_object" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="_method" value="delete" /><button name="button" type="submit">one_more_tag</button></form>'
-    expect(delete_button('/delete_this_object') { 'one_more_tag' }).to eq html
+    html = <<-HTML
+      <form action="/delete_this_object" accept-charset="UTF-8" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <input type="hidden" name="_method" value="delete" />
+        <button name="button" type="submit">one_more_tag</button>
+      </form>
+    HTML
+    expect(delete_button('/delete_this_object') { 'one_more_tag' }).to(
+      eq(html.to_s.gsub(/\n\s\s*/, '').gsub(/^\s\s*/, '').gsub("\n", ''))
+    )
   end
 
   it 'returns delete button HTML with form options' do
-    html = '<form class="some_class" action="/delete_this_object" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="_method" value="delete" /><button name="button" type="submit"></button></form>'
-    expect(delete_button('/delete_this_object', form_options: { class: :some_class })).to eq html
+    html = <<-HTML
+      <form class="some_class" action="/delete_this_object" accept-charset="UTF-8" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <input type="hidden" name="_method" value="delete" />
+        <button name="button" type="submit"></button>
+      </form>
+    HTML
+    expect(delete_button('/delete_this_object', form_options: { class: :some_class })).to(
+      eq(html.to_s.gsub(/\n\s\s*/, '').gsub(/^\s\s*/, '').gsub("\n", ''))
+    )
   end
 
   it 'returns delete button HTML with button options' do
-    html = '<form action="/delete_this_object" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="_method" value="delete" /><button name="button" type="submit" class="some_class"></button></form>'
-    expect(delete_button('/delete_this_object', button_options: { class: :some_class })).to eq html
+    html = <<-HTML
+      <form action="/delete_this_object" accept-charset="UTF-8" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <input type="hidden" name="_method" value="delete" />
+        <button name="button" type="submit" class="some_class"></button>
+      </form>
+    HTML
+    expect(delete_button('/delete_this_object', button_options: { class: :some_class })).to(
+      eq(html.to_s.gsub(/\n\s\s*/, '').gsub(/^\s\s*/, '').gsub("\n", ''))
+    )
   end
 end
