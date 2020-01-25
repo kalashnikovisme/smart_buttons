feab130035d5301dba2c5968b3e96ba15f0dc4d0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-26 00:19:25 +0400

    Rubocop at work

--- lib/smart_buttons.rb
+++ lib/smart_buttons.rb
@@ -5,7 +5,7 @@ require 'smart_buttons/version'
 module SmartButtons
   def delete_button(url, form_options: {}, button_options: {})
     form_tag url, method: :delete, **form_options do
-      button_tag **button_options do
+      button_tag(**button_options) do
         yield if block_given?
       end
     end
